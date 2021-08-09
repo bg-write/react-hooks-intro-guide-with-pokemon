78273cec5c1b256d7a020ee3e6e6fcd76bf8ba75
bg-write <bradywgerber@gmail.com>
2021-08-09 17:10:48 -0400

    Fix front_default load conditional

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -5,31 +5,32 @@ function Pokemon() {
 	// Set State!
 	const [count, setCount] = useState(1);
 	const [pokemon, setPokemon] = useState([]);
-
+	
 	// Define the function that'll fetch data from our API; we include "setPokemon" so that we can update State with the pokemon we pull from our API.
 	const fetchData = async () => {
 		const { data } = await axios.get(
 			`https://pokeapi.co/api/v2/pokemon/${count}`
-		);
-		setPokemon(data);
-	};
-
-	// Set "decreaseCount" to make sure we don't go below 1.
-	function decreaseCount() {
-		count <= 1 ? setCount(1) : setCount((preCount) => preCount - 1);
-	}
-	function increaseCount() {
-		setCount((preCount) => preCount + 1);
-	}
-
-	// Trigger our "fetchData" function after the initial State renders, so we can actually make our API call.
-	useEffect(() => {
-		fetchData();
-	}, [count]);
+			);
+			setPokemon(data);
+		};
+		
+		// Set "decreaseCount" to make sure we don't go below 1.
+		function decreaseCount() {
+			count <= 1 ? setCount(1) : setCount((preCount) => preCount - 1);
+		}
+		function increaseCount() {
+			setCount((preCount) => preCount + 1);
+		}
+		
+		// Trigger our "fetchData" function after the initial State renders, so we can actually make our API call.
+		useEffect(() => {
+			fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, [count]);
 
 	return (
 		<>
-			<h1>UPDATING API GET REQUEST BASED ON STATE</h1>
+			<h1>UPDATING OUR API GET REQUEST BASED ON STATE</h1>
 
 			<div className="counter">
 				<button onClick={decreaseCount}>-</button>
@@ -41,8 +42,7 @@ function Pokemon() {
 				<div className="content">
 					<div className="pokemon">
 						<h2>{pokemon.name}</h2>
-						{/* ICEBOX: image does not load on refresh. TypeError: Cannot read property 'front_default' of undefined */}
-						{/* <img alt="pokemon" src={pokemon.sprites.front_default} /> */}
+						{pokemon.length === 0 ? <p>loading ...</p> : <img alt="pokemon" src={pokemon.sprites.front_default} />}
 					</div>
 				</div>
 			</div>
